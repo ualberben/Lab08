9e8e54d7f1db2fb98f1d47ee560d2d682e50c553
ualberben <pinjui1@ualberta.ca>
2026-03-05 22:26:59 -0700

    Update CustomList.java

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -18,6 +18,8 @@ public class CustomList {
 
     public void deleteCity(City city){cities.remove(city);}
 
+    public int countCities(){return cities.size();}
+
     
 
     // Will be implemented later using TDD (leave blank for now)
